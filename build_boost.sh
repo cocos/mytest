fb3252aa5faaaaedf475836c2d561b6ea9f62312
Yun Hsiao Wu <yunhsiaow@gmail.com>
2019-03-31 23:51:50 +0800

    minor

--- build_boost.sh
+++ build_boost.sh
@@ -4,7 +4,7 @@ else
     mode=$1
 fi
 cd ext/boost
-. bootstrap.sh
+chmod +x bootstrap.sh && ./bootstrap.sh
 ./b2 headers
-./b2 address-model=$mode runtime-link=static stage --with-system --with-date_time --with-regex
+./b2 address-model=$mode link=static runtime-link=static stage --with-system --with-date_time --with-regex
 cd ../..
