4213f38d9a8ba41e4f7c7594aefe2e40b855f1cf
Myway <MSoft1115@hotmail.com>
2020-02-20 14:37:01 +0800

    解决release下的编译问题

--- build_boost.bat
+++ build_boost.bat
@@ -3,5 +3,5 @@ if [%1] == [] (set mode=32) else (set mode=%1)
 cd ext\boost
 call bootstrap.bat
 b2 headers
-b2 address-model=%mode% link=static runtime-link=static stage --with-system --with-date_time --with-regex
+b2 address-model=%mode% link=static runtime-link=static stage --with-system --with-date_time --with-regex --with-random
 cd ..\..
--- build_boost.sh
+++ build_boost.sh
@@ -6,5 +6,5 @@ fi
 cd ext/boost
 chmod +x bootstrap.sh && ./bootstrap.sh
 ./b2 headers
-./b2 address-model=$mode link=static runtime-link=static stage --with-system --with-date_time --with-regex
+./b2 address-model=$mode link=static runtime-link=static stage --with-system --with-date_time --with-regex --with-random
 cd ../..
