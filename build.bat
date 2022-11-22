edde1cb4370fa6fa87b8f4a15c00a5b172852ada
Myway3D <MSoft1115@hotmail.com>
2022-11-22 17:10:36 +0800

    添加解压命令

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-expand 3rd\embree\lib\embree.zip 3rd\embree\lib\embree.lib
-expand 3rd\embree\lib\embree_avx.zip 3rd\embree\lib\embree_avx.lib
-expand 3rd\embree\lib\embree_avx2.zip 3rd\embree\lib\embree_avx2.lib
-expand 3rd\embree\lib\embree_sse42.zip 3rd\embree\lib\embree_sse42.lib
+unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree.zip 
+unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx.zip 
+unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx2.zip 
+unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_sse42.zip 
 
 echo make solution
 cd build\win
 premake5 --file=premakex.lua --os=windows vs2019
 echo compile solution
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" bin/LightFX.sln /Rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" bin\LightFX.sln /Project LightFX /Build Release /Out build.log
