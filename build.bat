c4d96f920d95cfa67b99e068b19aa7f0793067ef
Myway3D <MSoft1115@hotmail.com>
2022-11-23 16:40:26 +0800

    修改构建命令

--- build.bat
+++ build.bat
@@ -1,10 +1,18 @@
-unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree.zip 
-unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx.zip 
-unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx2.zip 
-unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_sse42.zip 
+tools\unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree.zip 
+tools\unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx.zip 
+tools\unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_avx2.zip 
+tools\unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_sse42.zip 
 
 echo make solution
 cd build\win
 premake5 --file=premakex.lua --os=windows vs2019
+
 echo compile solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" bin\LightFX.sln /Project LightFX /Build Release /Out build.log
+cd ..\..\
+
+echo compress to target
+set datestr=%date:~0,4%%date:~5,2%%date:~8,2%
+set filename=build\lightmap-tools-win32\darwin-%datestr%.zip
+mkdir build\lightmap-tools-win32
+tools\zip -j %filename% build\win\bin\Release\LightFX.exe
