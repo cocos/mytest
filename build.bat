8aacb74afde55cc78ee7923bd6787f78b251d3cd
Myway3D <MSoft1115@hotmail.com>
2022-11-24 16:57:05 +0800

    修改编译错误

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ build\unzip -n -d 3rd\embree\lib 3rd\embree\lib\embree_sse42.zip
 
 echo make solution
 cd build\win
-premake5 --file=premakex.lua --os=windows vs2019
+premake5 --file=premake.lua --os=windows vs2019
 
 echo compile solution
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" bin\LightFX.sln /Project LightFX /Build Release /Out build.log
