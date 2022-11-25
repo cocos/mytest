799da89c4ee32bd3d3b4497f73aec0c87039966c
Myway3D <MSoft1115@hotmail.com>
2022-11-25 10:19:13 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 echo make solution
 cd build/mac
-sudo chmod 774 premake5
+chmod 774 premake5
 ./premake5 --os=macosx xcode4
-sudo chmod 774 bin
-sudo chmod -R 774 bin/*
+chmod 774 bin
+chmod -R 774 bin/*
 
 echo compile project
 xcodebuild -project bin/LightFX.xcodeproj -configuration Release
-sudo chmod -R 774 bin/Release/*
+chmod -R 774 bin/Release/*
 
 cd ../../
 echo compress to target
